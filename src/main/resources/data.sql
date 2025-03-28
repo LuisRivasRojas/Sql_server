25bf7ae3f833f36b08e64080ab610d8920b7a5dc
Luis Rivas <144482434+LuisRivasRojas@users.noreply.github.com>
2025-03-28 10:01:55 -0500

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 -- Crear el registro si es que no se duplican datos unicos o se insertan datos nulos
 INSERT INTO customer (dni, first_name, last_name, state)
 VALUES ('12345678', 'Juan', 'Sanchez', 'A');
+
+INSERT INTO sales (sale_date, quantity, payment_method)
+VALUES ('12-03-25', '4', 'Efectivo');
+
+INSERT INTO seller (dni, first_name, last_name, state)
+VALUES ('12345678', 'Antonio', 'Sanchez', 'A');
