5e404fb7d69d91936783edf52c284ba5f92efab6
Luis Rivas <144482434+LuisRivasRojas@users.noreply.github.com>
2025-03-28 10:01:28 -0500

    schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,18 @@ CREATE TABLE customer (
     last_name VARCHAR(50) NOT NULL,
     state CHAR(1) NOT NULL
 );
+
+CREATE TABLE sales (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    sale_date DATE, 
+    quantity INT,
+    payment_method VARCHAR(50) 
+);
+
+CREATE TABLE seller (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    dni CHAR(8) NOT NULL,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    state CHAR(1) NOT NULL
+);
